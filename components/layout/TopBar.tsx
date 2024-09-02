25ddde144bffa83285b0e6cf7e24e18832f62d7f
atifShaikh0 <maatif881@gamil.com>
2024-09-02 18:20:46 +0530

    Authentication and responsive Sidebar

---
+++ components/layout/TopBar.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { navLinks } from "@/lib/constants";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import logo from "../../public/Screenshot (52).png";
+import Link from "next/link";
+import { Menu } from "lucide-react";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+
+const TopBar = () => {
+  const [dropdownMenu, setDropdownMenu] = useState(false);
+  const pathname = usePathname();
+
+  return (
+    <div className="sticky top-0 z-20 w-full flex justify-between items-center px-8 py-4 b-blue-1 shadow-xl lg:hidden">
+      <Image src={logo} alt="logo" width={100} height={60} />
+      <h1>Supernal Admin</h1>
+
+      <div className="flex gap-8 max-md:hidden">
+        {navLinks.map((link) => (
+          <Link
+            href={link.url}
+            key={link.label}
+            className={`flex gap-4 text-body-medium ${
+              pathname === link.url ? "text-blue-1" : "text-grey-1"
+            }`}
+          >
+            <p>{link.label}</p>
+          </Link>
+        ))}
+      </div>
+      <div className="relative flex gap-4 items-center">
+        <Menu
+          className="cursor-pointer md:hidden"
+          onClick={() => setDropdownMenu(!dropdownMenu)}
+        />
+        {dropdownMenu && (
+          <div className="absolute top-10 right-6  flex flex-col gap-8 p-5 shadow-xl rounded-lg">
+            {navLinks.map((link) => (
+              <Link
+                href={link.url}
+                key={link.label}
+                className="flex gap-4 text-body-medium"
+              >
+                {link.icon} <p>{link.label}</p>
+              </Link>
+            ))}
+          </div>
+        )}
+        <UserButton />
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
